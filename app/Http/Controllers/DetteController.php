2bb523f37279b581d39c14a2e6d231eb6660d89d
elieoko <elieoko100@gmail.com>
2024-01-24 09:05:47 +0100

    fix bug

--- app/Http/Controllers/DetteController.php
+++ app/Http/Controllers/DetteController.php
@@ -16,13 +16,13 @@ class DetteController extends Controller
      */
     public function index()
     {
-        $data = Dette::with("stock.article","marketeur","type_vente","code","status")->orderBy('id', 'desc')->get();
-        if($data->count() != 0 ){
+        $data = Dette::with("stock.article", "marketeur", "type_vente", "code", "status")->orderBy('id', 'desc')->get();
+        if ($data->count() != 0) {
             return new DetteCollection($data);
         }
         return response()->json([
-            "message"=>"Ressource not found",
-        ],400);
+            "message" => "Ressource not found",
+        ], 400);
     }
 
     /**
@@ -45,51 +45,49 @@ class DetteController extends Controller
         $stock = Stock::find($dt->stock_fk);
         $difQteDispo = $stock->quantiteEntree - $stock->quantiteSortie;
         $state_save = false;
-        if($difQteDispo >= $dt->quantite_emprunter){
+        if ($difQteDispo >= $dt->quantite_emprunter) {
             $article    = Article::find($stock->article_fk);
             $price      = $dt->type_vente_fk == 1 ? $article->price_big : $article->prixUnitaire;
             // return response()->json([
             //     "montant "=>$article  ,
             //     "message"=>$msg,
             // ],$status);
-            $prixTotal  = $dt->quantite_emprunter * $price; 
+            $prixTotal  = $dt->quantite_emprunter * $price;
             $state_save = Dette::create([
                 "code_fk"                   => $dt->code_fk,
                 "stock_fk"                  => $dt->stock_fk,
                 "quantite_emprunter"        => $dt->quantite_emprunter,
                 "marketeur_fk"              => $dt->marketeur_fk,
-                "note"                      => $dt->note?? "",
+                "note"                      => $dt->note ?? "",
                 "type_vente_fk"             => $dt->type_vente_fk,
                 "date_creation"             => $day,
                 "status_fk"                 => 1,
                 "montant_final"             => $prixTotal
             ]);
             $stock = Stock::find($dt->stock_fk);
-            $stock->update(["quantiteSortie"=> $stock->quantiteSortie + $dt->quantite_emprunter]);
+            $stock->update(["quantiteSortie" => $stock->quantiteSortie + $dt->quantite_emprunter]);
             StockHistoriqueSortie::create([
                 "quantite"  => $dt->quantite_emprunter,
                 "stock_fk"  => $dt->stock_fk,
-                "prixUnitaire" => $dt->type_vente_fk == 1 ? $article->price_big : $article->prixUnitaire ,
+                "prixUnitaire" => $dt->type_vente_fk == 1 ? $article->price_big : $article->prixUnitaire,
                 "prixTotal"  => $prixTotal,
             ]);
             return response()->json([
-                "dettes"=>Dette::with("code","stock.article","status","type_vente","marketeur")->where('code_fk',$dt->code_fk??0)->get(),
-                "message"=>$msg,
-            ],$status);
-        }
-        else{
+                "dettes" => Dette::with("code", "stock.article", "status", "type_vente", "marketeur")->where('code_fk', $dt->code_fk ?? 0)->get(),
+                "message" => $msg,
+            ], $status);
+        } else {
             $msg = "Stock Insufisant pour la sortie de cette articles dans le stock";
-            if($difQteDispo != 0){
+            if ($difQteDispo != 0) {
                 $msg .= " disponible $difQteDispo";
-            }
-            else{
-                $msg = "Cette articles dans le stock est en rupture"; 
+            } else {
+                $msg = "Cette articles dans le stock est en rupture";
             }
             $status = 200;
-        } 
-            return response()->json([
-                "message"=>$msg,
-            ],$status);
+        }
+        return response()->json([
+            "message" => $msg,
+        ], $status);
     }
 
     /**
@@ -119,35 +117,33 @@ class DetteController extends Controller
         $dt                         = json_decode($request->getContent());
         $dette                      = Dette::find($id);
         $status                     = 2;
-        $quantite_vendu             = $dt->quantite_vendu??0;
-        $montant_payer              = $dt->montant_payer??0;
+        $quantite_vendu             = $dt->quantite_vendu ?? 0;
+        $montant_payer              = $dt->montant_payer ?? 0;
         $quantite_restante          = $dette->quantite_emprunter - $dt->quantite_vendu;
         $montant_restant            = floatval($dette->montant_final - $montant_payer);
-        if($dt->quantite_vendu > $dette->quantite_emprunter){
-            $msg    = "La quantité vendu ne peut pas être superieure à la quantité empreinter (".$dt->quantite_vendu.">".$dette->quantite_emprunter.")";
+        if ($dt->quantite_vendu > $dette->quantite_emprunter) {
+            $msg    = "La quantité vendu ne peut pas être superieure à la quantité empreinter (" . $dt->quantite_vendu . ">" . $dette->quantite_emprunter . ")";
             $state  = 401;
-        }
-        else if($montant_payer > $dette->montant_final){
-            $msg    = "Le montant payé ne peut pas être supérieur au montant final(".$montant_payer.">".$dette->montant_final.")";
+        } else if ($montant_payer > $dette->montant_final) {
+            $msg    = "Le montant payé ne peut pas être supérieur au montant final(" . $montant_payer . ">" . $dette->montant_final . ")";
             $state  = 401;
-        }
-        else{
-            if($montant_restant == 0 && $quantite_restante == 0){
+        } else {
+            if ($montant_restant == 0 && $quantite_restante == 0) {
                 $status = 3;
             }
             $update_data                = [
                 "quantite_vendu"    => $quantite_vendu,
                 "montant_payer"     => $montant_payer,
                 "quantite_restante" => $quantite_restante,
-                "montant_restant"   => $montant_restant,
+                "montant_restant"   => floatval($montant_restant),
                 "status_fk"         => $status
             ];
             $dette->update($update_data);
         }
-        $response =[
+        $response = [
             'message' => $msg
         ];
-        return response($response,$state);
+        return response($response, $state);
     }
 
     /**
